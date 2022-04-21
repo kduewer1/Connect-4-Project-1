e5b8212d402c5b14aceaf95e6cc9a94ceb65ab61
Kyle <102164046+kduewer1@users.noreply.github.com>
2022-04-21 14:25:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+list1 = ["-" , "-", "-", "-", "-", "-", "-"]
+list2 = ["-" , "-", "-", "-", "-", "-", "-"]
+list3 = ["-" , "-", "-", "-", "-", "-", "-"]
+list4 = ["-" , "-", "-", "-", "-", "-", "-"]
+list5 = ["-" , "-", "-", "-", "-", "-", "-"]
+list6 = ["-" , "-", "-", "-", "-", "-", "-"]
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+print(list5)
+print(list6)
+
+collumn = "B" + "B" + "B" + "B" + "B" + "B"
+for i in range(7):
+  print(collumn)
+
+print(list1)
+list1[5] = "hi"
+print(list1)
+
+#while True:
+  #player1 = int(input("What column woulod you like to place a red chip in?"))
+  #place(player1)
+  #winner()
+  #if winner == done:
+    #break
+  #player2 = int(input("What collumn would you like to place a black chip in?"))
+  #place(player2)
+  #winner()
+  #if winner == done:
+    #break
+
+  #place(x)
