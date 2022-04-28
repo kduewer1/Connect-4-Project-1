8f8dc01c564459897c846e6e5777cfed253f7239
Kyle <102164046+kduewer1@users.noreply.github.com>
2022-04-28 13:54:17 +0000

    change 2

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-list1 = ["-" , "-", "-", "-", "-", "-", "-"]
-list2 = ["-" , "-", "-", "-", "-", "-", "-"]
-list3 = ["-" , "-", "-", "-", "-", "-", "-"]
-list4 = ["-" , "-", "-", "-", "-", "-", "-"]
-list5 = ["-" , "-", "-", "-", "-", "-", "-"]
-list6 = ["-" , "-", "-", "-", "-", "-", "-"]
-print(list1)
-print(list2)
-print(list3)
-print(list4)
-print(list5)
-print(list6)
+guesslist = [1, 2, 7, 4, 25, 15, 12, 21, 16, 9]
+score = 0
+print("Welcome to the guessing game! To play, guess a number between 1 and 25. Guess all ten numbers correctly to win!")
+print("Also, don't guess the same number multiple times.")
+while score < 10:
+  try:
+    ask = int(input("What is your guess?: " ))
+  except ValueError:
+    print("Boi you gotta enter a number. Try again!")
+    continue
+  #if ask == guesslist[0] or ask == guesslist[1] or ask == guesslist[2] or ask == guesslist[3] or ask == guesslist[4] or ask == guesslist[5] or ask == guesslist[6] or ask == guesslist[7] or ask == guesslist[8] or ask == guesslist[9]:
+  guesslist = set(guesslist)
+  if ask in guesslist:
+    print("You guessed correctly! You got a point!")
+    print("The number was " + str(ask))
+    score = score + 1
+    print("Score: " + str(score))
+  elif ask > 25:
+    print("That number is too high! Try again!")
+  elif ask < 1:
+    print("That number is too low! Try agian!")
+  else:
+    print("Incorrect guess. Try again!")
 
-collumn = "B" + "B" + "B" + "B" + "B" + "B"
-for i in range(7):
-  print(collumn)
-
-print(list1)
-list1[5] = "hi"
-print(list1)
-
-#while True:
-  #player1 = int(input("What column woulod you like to place a red chip in?"))
-  #place(player1)
-  #winner()
-  #if winner == done:
-    #break
-  #player2 = int(input("What collumn would you like to place a black chip in?"))
-  #place(player2)
-  #winner()
-  #if winner == done:
-    #break
-
-  #place(x)
-print("hi")
-print("Mark's test + Kyle's test")
+print("Congrats! You won!")
